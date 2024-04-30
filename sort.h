daba0e0dd87abb8922658ef73d39727e2081ed5e
Houssame Ait Hsain <houssame1337@gmail.com>
2024-04-30 17:21:29 +0100

    quick%

--- sort.h
+++ sort.h
@@ -25,6 +25,30 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_sort_recursion(int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
+void swap(int *a, int *b);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge_sort_recursion(int *array, int low, int high, size_t size);
+void merge(int *array, int low, int mid, int high, size_t size);
+void heap_sort(int *array, size_t size);
+void heapify(int *array, int n, int i, size_t size);
+void radix_sort(int *array, size_t size);
+void counting_sort_radix(int *array, size_t size, int exp);
+void bitonic_sort(int *array, size_t size);
+void bitonic_sort_recursion(int *array, int low,
+		int count, int dir, size_t size);
+void bitonic_merge(int *array, int low, int count, int dir, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void quick_sort_hoare_recursion(int *array, int low, int high, size_t size);
+int partition_hoare(int *array, int low, int high, size_t size);
+void swap_hoare(int *a, int *b);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void counting_sort_radix(int *array, size_t size, int exp);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 
 #endif /* SORT_H */
